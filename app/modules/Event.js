ba25e4899556d6a7f4c3f43f6085771a07a6a553
jbooher <john.c.booher@gmail.com>
2016-03-31 17:04:34 -0500

    Adventurer mode done.  Moving on to epic mode!

--- app/modules/Event.js
+++ app/modules/Event.js
@@ -16,11 +16,9 @@ class Event {
         this.title = response.data.results[0].title;
         this.description = response.data.results[0].description;
         this.image = `${response.data.results[0].thumbnail.path}.${response.data.results[0].thumbnail.extension}`;
-        this.characters = response.data.results[0].characters.items;
-        console.log(this.characters);
 
         this.render();
-        this.renderCharacters();
+        this.getCharacterData();
       })
   }
 
@@ -28,6 +26,20 @@ class Event {
     document.querySelector(".content--profile-pic").src = this.image;
     document.querySelector(".content--profile-title").innerHTML = this.title;
     document.querySelector(".content--profile-description").innerHTML = this.description;
+  }
+
+  getCharacterData() {
+    const token = `6e7bd33438a14b84d91097cd3cfc46b5`;
+
+    fetch(`http://gateway.marvel.com/v1/public/events/${this.id}/characters?apikey=${token}`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        this.characters = response.data.results;
+
+        this.renderCharacters();
+      });
   }
 
   renderCharacters() {
@@ -40,15 +52,18 @@ class Event {
       let div = document.createElement("div");
       div.classList.add("content--character");
 
-      // let img = document.createElement("img");
-      // img.src = `${event.thumbnail.path}.${event.thumbnail.extension}`;
-      // div.appendChild(img);
-
       let a = document.createElement("a");
       a.href = `/characters/${character.name}`;
-      a.innerHTML = character.name;
-      div.appendChild(a);
 
+      let img = document.createElement("img");
+      img.src = `${character.thumbnail.path}.${character.thumbnail.extension}`;
+      a.appendChild(img);
+
+      let label = document.createElement("label");
+      label.innerHTML = character.name;
+      a.appendChild(label);
+
+    div.appendChild(a);
     placeholder.appendChild(div);
     });
   }
